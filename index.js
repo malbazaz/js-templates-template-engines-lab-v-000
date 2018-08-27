d503ca88356de4310b445f1f8dcfc02c1ab47fea
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-27 13:02:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,6 @@
+// create template of id page-template
+// that will represent the page of a blog post
+// including a sidebar element and post element
+function createPost(){
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML)
+}
